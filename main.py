9a0e625f2c3a6e82ff1b2ea68f259aa28c9ba1cf
David Quiceno <davidquiceno69@gmail.com>
2026-05-01 15:22:29 -0500

    subida de imagenes a S3 para cuando este listo

--- main.py
+++ main.py
@@ -19,6 +19,16 @@ sqs = boto3.client(
     aws_session_token=os.getenv("AWS_SESSION_TOKEN")
 )
 
+s3 = boto3.client(
+    "s3",
+    region_name="us-east-1",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    aws_session_token=os.getenv("AWS_SESSION_TOKEN")
+)
+
+BUCKET = os.getenv("S3_BUCKET")
+
 app = FastAPI()
 
 UPLOAD_DIR = "images"
@@ -29,6 +39,7 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 async def upload_image(file: UploadFile = File(...)):
     filename = f"{uuid.uuid4()}_{file.filename}"
     file_path = os.path.join(UPLOAD_DIR, filename)
+    # s3.upload_file(file_path, BUCKET, filename)   (Cambiar la línea de arriba por esto cuando este lo de S3)
 
     with open(file_path, "wb") as buffer:
         buffer.write(await file.read())
