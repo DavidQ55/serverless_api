205596b319c5bdf0063c865f39ef0b2901325d9f
David Quiceno <davidquiceno69@gmail.com>
2026-05-01 14:43:01 -0500

    api conectada a SQS con función Lambda en AWS

---
+++ main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, UploadFile, File, HTTPException
+import os
+import boto3
+import json
+import uuid
+from dotenv import load_dotenv
+
+load_dotenv()
+
+QUEUE_URL = os.getenv("QUEUE_URL")
+if not QUEUE_URL:
+    raise ValueError("QUEUE_URL no está definida en el .env")
+
+sqs = boto3.client(
+    'sqs',
+    region_name='us-east-1',
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    aws_session_token=os.getenv("AWS_SESSION_TOKEN")
+)
+
+app = FastAPI()
+
+UPLOAD_DIR = "images"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+
+@app.post("/upload-image")
+async def upload_image(file: UploadFile = File(...)):
+    filename = f"{uuid.uuid4()}_{file.filename}"
+    file_path = os.path.join(UPLOAD_DIR, filename)
+
+    with open(file_path, "wb") as buffer:
+        buffer.write(await file.read())
+
+    mensaje = {
+        "file_name": filename
+    }
+
+    try:
+        sqs.send_message(
+            QueueUrl=QUEUE_URL,
+            MessageBody=json.dumps(mensaje)
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return {
+        "message": "Imagen enviada a procesamiento",
+        "filename": filename
+    }
+
+
+@app.get("/")
+def home():
+    return {"status": "API funcionando"}
